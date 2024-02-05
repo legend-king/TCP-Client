1659cd56297916babb2e05cdfed0d83453dcae9c
mustafaabdullabhaik <mustafakhorakiwala53@gmail.com>
2024-02-05 15:15:43 +0530

    server created

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,29 @@
 package org.example
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.PrintWriter
 import java.net.ServerSocket
 import java.net.Socket
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
+    Thread{ server() }.start()
+}
+
+fun server() {
+    val server = ServerSocket(9999)
+
+    val client = server.accept()
+    while (true){
+        val output = PrintWriter(client.getOutputStream(), true)
+        val input = BufferedReader(InputStreamReader(client.inputStream))
+        val content = input.readLine()
+        if (content.equals("Exit")){
+            break
+        }
+        output.println(content)
+    }
+    client.close()
 
 }
